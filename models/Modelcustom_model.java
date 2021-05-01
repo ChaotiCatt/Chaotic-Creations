261cab3f19f18b9492875ef535709f3464473b8a
ChaotiCatt <kaiic@10.0.0.24>
2021-04-30 17:44:09 -0700

    wfawegvewa

---
+++ models/Modelcustom_model.java
@@ -0,0 +1,114 @@
+// Made with Blockbench 3.8.4
+// Exported for Minecraft version 1.15 - 1.16
+// Paste this class into your mod and generate all required imports
+
+public static class Modelcustom_model extends EntityModel<Entity> {
+	private final ModelRenderer head;
+	private final ModelRenderer cube_r1;
+	private final ModelRenderer cube_r2;
+	private final ModelRenderer cube_r3;
+	private final ModelRenderer body;
+	private final ModelRenderer upperBody;
+	private final ModelRenderer leg0;
+	private final ModelRenderer leg1;
+	private final ModelRenderer leg2;
+	private final ModelRenderer leg3;
+	private final ModelRenderer tail;
+
+	public Modelcustom_model() {
+		textureWidth = 64;
+		textureHeight = 64;
+
+		head = new ModelRenderer(this);
+		head.setRotationPoint(-1.0F, 13.5F, -7.0F);
+		head.setTextureOffset(0, 0).addBox(-3.0F, -3.0F, -2.0F, 6.0F, 6.0F, 4.0F, 0.0F, false);
+		head.setTextureOffset(16, 14).addBox(-3.0F, -5.0F, 0.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+		head.setTextureOffset(16, 14).addBox(1.0F, -5.0F, 0.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+		head.setTextureOffset(0, 10).addBox(-1.5F, -0.0156F, -5.0F, 3.0F, 3.0F, 4.0F, 0.0F, false);
+
+		cube_r1 = new ModelRenderer(this);
+		cube_r1.setRotationPoint(1.0F, 10.5F, 7.0F);
+		head.addChild(cube_r1);
+		setRotationAngle(cube_r1, -1.5708F, 0.0F, 0.0F);
+		cube_r1.setTextureOffset(0, 55).addBox(-3.0F, 7.0F, -8.0F, 4.0F, 6.0F, 1.0F, 0.0F, false);
+
+		cube_r2 = new ModelRenderer(this);
+		cube_r2.setRotationPoint(1.0F, 10.5F, 7.0F);
+		head.addChild(cube_r2);
+		setRotationAngle(cube_r2, -1.4835F, 0.0F, 0.0F);
+		cube_r2.setTextureOffset(0, 55).addBox(-3.0F, 6.0F, -12.0F, 4.0F, 6.0F, 2.0F, 0.0F, false);
+
+		cube_r3 = new ModelRenderer(this);
+		cube_r3.setRotationPoint(1.0F, 10.5F, 7.0F);
+		head.addChild(cube_r3);
+		setRotationAngle(cube_r3, -1.1345F, 0.0F, 0.0F);
+		cube_r3.setTextureOffset(0, 55).addBox(2.0F, -1.0F, -16.0F, 1.0F, 6.0F, 4.0F, 0.0F, false);
+		cube_r3.setTextureOffset(0, 55).addBox(-5.0F, -1.0F, -16.0F, 1.0F, 6.0F, 4.0F, 0.0F, false);
+		cube_r3.setTextureOffset(0, 55).addBox(-4.0F, -1.0F, -16.5F, 6.0F, 6.0F, 1.0F, 0.0F, false);
+
+		body = new ModelRenderer(this);
+		body.setRotationPoint(0.0F, 14.0F, 2.0F);
+		setRotationAngle(body, 1.5708F, 0.0F, 0.0F);
+		body.setTextureOffset(18, 14).addBox(-4.0F, -2.0F, -3.0F, 6.0F, 9.0F, 6.0F, 0.0F, false);
+		body.setTextureOffset(0, 55).addBox(-4.0F, -2.0F, 3.0F, 6.0F, 4.0F, 1.0F, 0.0F, false);
+		body.setTextureOffset(0, 55).addBox(-5.0F, -2.0F, -2.0F, 1.0F, 3.0F, 6.0F, 0.0F, false);
+		body.setTextureOffset(0, 55).addBox(2.0F, -2.0F, -2.0F, 1.0F, 3.0F, 6.0F, 0.0F, false);
+
+		upperBody = new ModelRenderer(this);
+		upperBody.setRotationPoint(-1.0F, 14.0F, 2.0F);
+		setRotationAngle(upperBody, -1.5708F, 0.0F, 0.0F);
+		upperBody.setTextureOffset(21, 0).addBox(-4.0F, 2.0F, -4.0F, 8.0F, 6.0F, 7.0F, 0.0F, false);
+		upperBody.setTextureOffset(0, 55).addBox(-5.0F, 2.0F, -4.0F, 1.0F, 6.0F, 6.0F, 0.0F, false);
+		upperBody.setTextureOffset(0, 55).addBox(4.0F, 2.0F, -4.0F, 1.0F, 6.0F, 6.0F, 0.0F, false);
+		upperBody.setTextureOffset(0, 55).addBox(-4.0F, 2.0F, -5.0F, 8.0F, 6.0F, 1.0F, 0.0F, false);
+
+		leg0 = new ModelRenderer(this);
+		leg0.setRotationPoint(-2.5F, 16.0F, 7.0F);
+		leg0.setTextureOffset(0, 18).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F, false);
+
+		leg1 = new ModelRenderer(this);
+		leg1.setRotationPoint(0.5F, 16.0F, 7.0F);
+		leg1.setTextureOffset(0, 18).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F, false);
+
+		leg2 = new ModelRenderer(this);
+		leg2.setRotationPoint(-2.5F, 16.0F, -4.0F);
+		leg2.setTextureOffset(0, 18).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F, false);
+
+		leg3 = new ModelRenderer(this);
+		leg3.setRotationPoint(0.5F, 16.0F, -4.0F);
+		leg3.setTextureOffset(0, 18).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F, false);
+
+		tail = new ModelRenderer(this);
+		tail.setRotationPoint(-1.0F, 12.0F, 8.0F);
+		setRotationAngle(tail, 0.9599F, 0.0F, 0.0F);
+		tail.setTextureOffset(9, 18).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F, false);
+		tail.setTextureOffset(38, 54).addBox(-1.0F, 1.0F, 1.0F, 2.0F, 7.0F, 1.0F, 0.0F, false);
+		tail.setTextureOffset(38, 54).addBox(-2.0F, 1.0F, -1.0F, 1.0F, 7.0F, 2.0F, 0.0F, false);
+		tail.setTextureOffset(38, 54).addBox(1.0F, 1.0F, -1.0F, 1.0F, 7.0F, 2.0F, 0.0F, false);
+	}
+
+	@Override
+	public void setRotationAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks,
+			float netHeadYaw, float headPitch) {
+		// previously the render function, render code was moved to a method below
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red,
+			float green, float blue, float alpha) {
+		head.render(matrixStack, buffer, packedLight, packedOverlay);
+		body.render(matrixStack, buffer, packedLight, packedOverlay);
+		upperBody.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg0.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg1.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg2.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg3.render(matrixStack, buffer, packedLight, packedOverlay);
+		tail.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+}
