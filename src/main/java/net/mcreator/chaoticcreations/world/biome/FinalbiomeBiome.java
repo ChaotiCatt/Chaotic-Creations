258e68561e71e000f0684df510aebcafc1250f5c
ChaotiCatt <kaiic@10.0.0.13>
2021-07-18 22:57:19 -0700

    peepeepoopoo check

--- src/main/java/net/mcreator/chaoticcreations/world/biome/FinalbiomeBiome.java
+++ src/main/java/net/mcreator/chaoticcreations/world/biome/FinalbiomeBiome.java
@@ -5,7 +5,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.common.BiomeManager;
 
 import net.minecraft.world.gen.trunkplacer.StraightTrunkPlacer;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
@@ -26,9 +25,6 @@ import net.minecraft.world.biome.DefaultBiomeFeatures;
 import net.minecraft.world.biome.BiomeGenerationSettings;
 import net.minecraft.world.biome.BiomeAmbience;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.util.registry.WorldGenRegistries;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.RegistryKey;
 import net.minecraft.block.Blocks;
 
 import net.mcreator.chaoticcreations.block.NullstoneBlock;
@@ -78,7 +74,5 @@ public class FinalbiomeBiome extends ChaoticCreationsModElements.ModElement {
 	}
 	@Override
 	public void init(FMLCommonSetupEvent event) {
-		BiomeManager.addBiome(BiomeManager.BiomeType.WARM,
-				new BiomeManager.BiomeEntry(RegistryKey.getOrCreateKey(Registry.BIOME_KEY, WorldGenRegistries.BIOME.getKey(biome)), 10));
 	}
 }
